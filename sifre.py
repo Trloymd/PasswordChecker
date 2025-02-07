65eab4234f36b499e3663fbb975ceebc157f1479
Trloymd <alptrloymd2000@gmail.com>
2025-02-07 22:25:23 +0300

    Update sifre.py

--- sifre.py
+++ sifre.py
@@ -7,7 +7,7 @@ length = int(input("Şifre Kaç Haneli Olsun (8den fazla olmalı): "))
 if length < 8:
     print("Şifre uzunluğu en az 8 olmalıdır!")
 else:
-    sifre = input("Şifreni yaz: ")
+    sifre = ""
 
     while len(sifre) < length:
         sifre += random.choice(klavye)
